08cefd170c9024ea26a7e0e50945fde7ed8be0a8
toyota-m2k <toyota.mitsuki@gmail.com>
2024-07-08 17:54:49 +0900

    chore: version catalog

--- libMedia/build.gradle.kts
+++ libMedia/build.gradle.kts
@@ -42,13 +42,13 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-    implementation("com.github.toyota-m2k:android-utilities:2.0.2")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.coroutinesCore)
+    implementation(libs.android.utilities)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.junitExt)
+    androidTestImplementation(libs.espressoCore)
 }
 
 // ./gradlew publishToMavenLocal
