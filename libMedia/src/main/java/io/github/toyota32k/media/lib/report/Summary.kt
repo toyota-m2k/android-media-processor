678597dc89acb8af0dd86f12f887e1ae92c0393c
toyota-m2k <toyota.mitsuki@gmail.com>
2026-08-07 17:06:51 +0900

    feat: add fastStart status, calc'd iFrameInterval to Summary

--- libMedia/src/main/java/io/github/toyota32k/media/lib/report/Summary.kt
+++ libMedia/src/main/java/io/github/toyota32k/media/lib/report/Summary.kt
@@ -1,10 +1,14 @@
 package io.github.toyota32k.media.lib.report
 
+import android.media.MediaExtractor
 import io.github.toyota32k.media.lib.legacy.converter.Converter
 import io.github.toyota32k.media.lib.io.IInputMediaFile
 import io.github.toyota32k.media.lib.legacy.extractor.Extractor
 import io.github.toyota32k.media.lib.format.MetaData
 import io.github.toyota32k.media.lib.format.bitRate
+import io.github.toyota32k.media.lib.io.AndroidFile
+import io.github.toyota32k.media.lib.processor.optimizer.FastStart
+import io.github.toyota32k.media.lib.types.RangeUs.Companion.formatAsMs
 import io.github.toyota32k.media.lib.utils.TimeSpan
 import java.util.Locale
 
@@ -13,6 +17,8 @@ data class Summary (
     var duration:Long = 0L, //MS
     var videoSummary: VideoSummary? = null,
     var audioSummary: AudioSummary? = null,
+    var fastStartCheck: FastStart.CheckResult? = null,
+    val iFrameIntervalCalced:Long = -1L
 ) : IAttributes {
     private fun stringInKb(size: Long): String {
         return if(size<0) {
@@ -30,6 +36,8 @@ data class Summary (
         return listOf(
             IAttributes.KeyValue("File Size", stringInKb(size)),
             IAttributes.KeyValue("Duration", TimeSpan(duration).formatH()),
+            IAttributes.KeyValue("Fast Start", fastStartCheck?.toString() ?: "uav"),
+            IAttributes.KeyValue("iFrameInterval(calc)", iFrameIntervalCalced.formatAsMs())
         )
     }
 
@@ -45,7 +53,25 @@ data class Summary (
     }
 
     companion object {
+        private fun calcIFrameInterval(extractor: MediaExtractor, trackIndex:Int):Long {
+            extractor.selectTrack(trackIndex)
+            extractor.seekTo(0, MediaExtractor.SEEK_TO_CLOSEST_SYNC)
+            var prev = extractor.sampleTime
+            var interval = 0L
+            for(i in 0..10) {
+                extractor.seekTo(prev + 1, MediaExtractor.SEEK_TO_NEXT_SYNC)
+                val t = extractor.sampleTime
+                if(t <= prev) break
+                interval = t - prev
+                prev = t
+            }
+            return interval /1000L
+        }
+
         fun getSummary(inFile:IInputMediaFile):Summary {
+            val fastStartCheck = if (inFile is AndroidFile) {
+                FastStart.check(inFile)
+            } else null
             return inFile.openExtractor().useObj { extractor ->
                 val metaData = MetaData.fromFile(inFile)
                 val videoTrack = Extractor.findTrackIdx(extractor, "video")
@@ -54,12 +80,13 @@ data class Summary (
                     Converter.logger.info("BitRate = ${videoFormat.bitRate ?: -1L} / ${metaData.bitRate ?: -1L}")
                     VideoSummary(null, videoFormat, metaData)
                 } else null
+                val iFrameInterval = calcIFrameInterval(extractor, videoTrack)
                 val audioTrack = Extractor.findTrackIdx(extractor, "audio")
                 val audioSummary = if (audioTrack >= 0) {
                     AudioSummary(null, Extractor.getMediaFormat(extractor, audioTrack))
                 } else null
                 val duration = metaData.duration ?: -1L
-                Summary(inFile.getLength(), duration, videoSummary, audioSummary)
+                Summary(inFile.getLength(), duration, videoSummary, audioSummary, fastStartCheck, iFrameInterval)
             }
         }
     }
